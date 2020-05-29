59e6d7bacae478f2db1d4d99627399ce7e3fcd5c
Vini Black <viniibl4ck@gmail.com>
2020-05-29 14:34:06 -0300

    criando uma classe abstrata para nao conseguir instanciar Conta Diretamente

--- Conta.js
+++ Conta.js
@@ -1,14 +1,14 @@
 export class Conta {
  constructor(saldoInicial, cliente, agencia) {
 
+  if (this.constructor == Conta) {
+   throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente");
+
+  }
+
   this._saldo = saldoInicial;
   this._cliente = cliente;
   this._agencia = agencia;
-
-  if (this.constructor == Conta) {
-   console.log("Você não deveria instanciar um objeto do tipo conta");
-
-  }
  }
 
  sacar(valor) {
--- index.js
+++ index.js
@@ -8,5 +8,3 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-
-
